f03bd941b1d41c854d8f36631c51165cb55ded64
Marvin Ochsenius <marvin.ochsenius@gmail.com>
2020-11-25 15:24:28 +0100

    Create warpper function for check_disk_full

--- all_checks.py
+++ all_checks.py
@@ -6,11 +6,19 @@ def check_reboot():
 	"""Returns True if the computer has a pending reboot."""
 	return os.path.exists("/run/reboot-required")
 
+def check_root_full():
+	"""Returns True if the root partition is full, False otherwise."""
+	return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
 def main():
 	if check_reboot():
 
 		print("Pending Reboot!")
 		sys.exit(1)
+	if check_root_full():
+		print("Root partition full.")
+		sys.exit(1)
+	
 	print("Everything ok.")
 	sys.exit(0)
 
